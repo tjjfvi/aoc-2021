647ab3d14cf929bdf8ab5578b8d7c1fe9b4c0c25
tjjfvi <tjjfvi@tjjfvi.com>
2021-12-01 09:36:04 -0500

    day 1 solutions

---
+++ solutions/2021-01-1.ts
@@ -0,0 +1,44 @@
+
+import { aoc, solution, test, dbg } from "../host/lib"
+import { add, mult, raceGens, allGens, repeatTillConst } from "./helpers"
+
+aoc(2021, 1, 1)
+
+test(`
+
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+
+
+`, (
+  7
+), (
+  -1
+))
+
+test(`
+
+
+
+`, (
+  -1
+), (
+  -1
+))
+
+solution(async input => {
+  let lines = input.split("\n")
+  let paragraphs = input.split("\n\n")
+
+  return lines.map(x => +x).filter((x, i, a) => x > a[i - 1]).length
+
+  return 0
+})
---
+++ solutions/2021-01-2.ts
@@ -0,0 +1,44 @@
+
+import { aoc, solution, test, dbg } from "../host/lib"
+import { add, mult, raceGens, allGens, repeatTillConst } from "./helpers"
+
+aoc(2021, 1, 2)
+
+test(`
+
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+
+
+`, (
+  7
+), (
+  5
+))
+
+test(`
+
+
+
+`, (
+  -1
+), (
+  -1
+))
+
+solution(async input => {
+  let lines = input.split("\n")
+  let paragraphs = input.split("\n\n")
+
+  return lines.map(x => +x).map((x, i, a) => a.slice(i - 3, i).reduce(add, 0)).filter((x, i, a) => x > a[i - 1]).length
+
+  return 0
+})
